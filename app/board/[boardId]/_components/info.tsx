3b5ecd59918096622ae76971aaa6b5db4b21ac2b
Riki321 <rickyparikh35@gmail.com>
2024-03-26 15:16:04 +0530

    collebration feature implemented, basic layout for workspece define,minor bug fix

---
+++ app/board/[boardId]/_components/info.tsx
@@ -0,0 +1,16 @@
+export const Info=()=>{
+    return (
+        <main className="absolute top-2 left-2 bg-white rounded-md
+        px-1.5 h-12 flex items-center shadow-md">
+            TODO:Info about the board
+        </main>
+    );
+};
+export const InfoSkeleton=()=>{
+    return (
+        <div className="absolute top-2 left-2 bg-white rounded-md
+        px-1.5 h-12 flex items-center shadow-md"/>
+
+        
+    );
+};
