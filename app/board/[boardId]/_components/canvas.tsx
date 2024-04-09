6602da9cd0db2cf6f0cefdb9565a71c2b8d297e2
Riki321 <rickyparikh35@gmail.com>
2024-04-09 15:04:07 +0530

    resizing, traslating of shape squre implemented, minor bug while sliding mouse pointer is fix

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -13,6 +13,7 @@ import { nanoid } from "nanoid";
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
+import { set } from "date-fns";
 const MAX_LAYERS = 100;
 
 
@@ -67,35 +68,70 @@ export const Canvas = ({
         setCanvasState({ mode: CanvasMode.None });
     }, [lastUsedColor]);
 
-    //
-    const resizeSelectedLayer = useMutation((
-        {storage,self},
-        point: Point,
-    )=>{
-        if(canvasState.mode!==CanvasMode.Resizing){
-            return;
-        }
-        const bounds=resizeBounds(canvasState.initialBounds,canvasState.corner,point);
-        const liveLayers=storage.get("layers");
-        const layer=liveLayers.get(self.presence.selection[0]);
-        if(!layer){
-            return;
-        }
-    
-    },[canvasState]);
 
     // 
-    const onResizeHandlePointerDown=useCallback((
-        corner:Side,
-        initialBounds:XYWH,
+    const translateSelectedLayers = useMutation((
+        { storage, self },
+        point: Point,
+    ) => {
+        if (canvasState.mode !== CanvasMode.Translating) {
+            return;
+        }
+        const offset = {
+            x: point.x - canvasState.current.x,
+            y: point.y - canvasState.current.y,
+        };
+        const liveLayers = storage.get("layers");
+        for (const layerId of self.presence.selection) {
+            const layer = liveLayers.get(layerId);
+            if (layer) {
+                layer.update({
+                    x: layer.get("x") + offset.x,
+                    y: layer.get("y") + offset.y,
+                });
+            }
+        }
+        setCanvasState({ mode: CanvasMode.Translating, current: point });
+    }, [canvasState,]);
+    //
+
+    const unselecteLayers=useMutation((
+        {self, setMyPresence},
     )=>{
+        if(self.presence.selection.length>0){
+            setMyPresence({selection:[]},{addToHistory:true});
+        }
+    },[]);
+
+    // 
+    const resizeSelectedLayer = useMutation((
+        { storage, self },
+        point: Point,
+    ) => {
+        if (canvasState.mode !== CanvasMode.Resizing) {
+            return;
+        }
+        const bounds = resizeBounds(canvasState.initialBounds, canvasState.corner, point);
+        const liveLayers = storage.get("layers");
+        const layer = liveLayers.get(self.presence.selection[0]);
+        if (!layer) {
+            return;
+        }
+
+    }, [canvasState]);
+
+    // 
+    const onResizeHandlePointerDown = useCallback((
+        corner: Side,
+        initialBounds: XYWH,
+    ) => {
         history.pause();
         setCanvasState({
-            mode:CanvasMode.Resizing,
+            mode: CanvasMode.Resizing,
             initialBounds,
             corner,
         })
-    },[history])
+    }, [history])
 
 
 
@@ -113,12 +149,14 @@ export const Canvas = ({
         e.preventDefault();
         const current = pointerEventToCanvasPoint(e, camera);
         // console.log(current);
-        if(canvasState.mode===CanvasMode.Resizing)
-        {
+        if (canvasState.mode === CanvasMode.Translating) {
+            translateSelectedLayers(current);
+        }
+        else if (canvasState.mode === CanvasMode.Resizing) {
             resizeSelectedLayer(current);
         }
         setMyPresence({ cursor: current });
-    }, [canvasState,camera,resizeSelectedLayer]);
+    }, [canvasState, camera, resizeSelectedLayer, translateSelectedLayers]);
 
     const onPointerLeave = useMutation((
         { setMyPresence }
@@ -127,20 +165,39 @@ export const Canvas = ({
     }, [canvasState]);
 
 
+
+    // 
+    const onPointerDown = useCallback((
+        e: React.PointerEvent,
+    ) => {
+        const point = pointerEventToCanvasPoint(e, camera);
+        if (canvasState.mode === CanvasMode.Inserting) {
+            return;
+        }
+        // TODO:ADD CASE FOR DRAWING
+
+        setCanvasState({ mode: CanvasMode.Pressing, origin: point });
+    }, [camera, canvasState.mode, setCanvasState]);
+
     // 
     const onPointerUp = useMutation((
         { },
         e
     ) => {
         const point = pointerEventToCanvasPoint(e, camera);
-        if (canvasState.mode === CanvasMode.Inserting) {
+
+        if (canvasState.mode === CanvasMode.Pressing || canvasState.mode === CanvasMode.None) {
+            unselecteLayers();
+            setCanvasState({ mode: CanvasMode.None });
+        }
+        else if (canvasState.mode === CanvasMode.Inserting) {
             insertLayer(canvasState.layerType, point);
         }
         else {
             setCanvasState({ mode: CanvasMode.None });
         }
         history.resume();
-    }, [camera, canvasState, history, insertLayer]);
+    }, [camera, canvasState, history, insertLayer, unselecteLayers]);
 
     const selections = useOthersMapped((other) => other.presence.selection);
 
@@ -195,6 +252,7 @@ export const Canvas = ({
                 onPointerMove={onPointerMove}
                 onPointerLeave={onPointerLeave}
                 onPointerUp={onPointerUp}
+                onPointerDown={onPointerDown}
             >
                 <g
                     style={{
