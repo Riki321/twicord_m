b8d427c6c1ac8af157f42a24ba9863dfca4659f9
Riki321 <rickyparikh35@gmail.com>
2024-04-15 14:57:00 +0530

    added a pencil tool completly for users and teammetes, fixing some color bugs in canvas

--- app/board/[boardId]/_components/cursors-presence.tsx
+++ app/board/[boardId]/_components/cursors-presence.tsx
@@ -7,9 +7,10 @@ import {
   useOthersConnectionIds, 
   useOthersMapped
 } from "@/liveblocks.config";
-
+import { colorToCss } from "@/lib/utils";
 
 import { Cursor } from "./cursor";
+import { Path } from "./path";
 
 const Cursors = () => {
   const ids = useOthersConnectionIds();
@@ -26,12 +27,37 @@ const Cursors = () => {
   );
 };
 
+const Drafts = () => {
+  const others = useOthersMapped((other) => ({
+    pencilDraft: other.presence.pencilDraft,
+    penColor: other.presence.penColor,
+  }), shallow);
 
+  return (
+    <>
+      {others.map(([key, other]) => {
+        if (other.pencilDraft) {
+          return (
+            <Path
+              key={key}
+              x={0}
+              y={0}
+              points={other.pencilDraft}
+              fill={other.penColor ? colorToCss (other.penColor) : "#000"}
+            />
+          );
+        }
+
+        return null;
+      })}
+    </>
+  )
+}
 
 export const CursorsPresence = memo(() => {
   return (
     <>
-      
+      <Drafts />
       <Cursors />
     </>
   );
